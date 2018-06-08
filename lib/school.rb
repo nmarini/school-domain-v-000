ad211a2bf8b4bf3aedc46c2b044aa569d0c0ea90
Nicolas Marini <nmarini44@gmail.com>
2018-06-08 00:54:45 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,7 +24,8 @@ class School
   def sort
   sort_roster = {}
     @roster.map do |grade, student|
-    sort_roster[grade].sort ||= []
+    sort_roster[grade] ||= []
+    sort_roster[grade] = grade.sort
     sort_roster[grade].sort << student.sort
     end
     sort_roster
